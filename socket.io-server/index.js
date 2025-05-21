99528e3d93c7e91a100a8425626854f930e04fef
Martins Zeltins <queeeeenz@gmail.com>
2025-05-21 08:18:17 +0300

    Initial commit

--- socket.io-server/index.js
+++ socket.io-server/index.js
@@ -1,16 +1,32 @@
 import express from 'express'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import { instrument } from '@socket.io/admin-ui'
 
 const app = express()
 const server = createServer(app)
 
 const io = new Server(server, {
     path: '/ws',
+    cors: {
+        origin: (origin, callback) => {
+            if (origin) {
+                callback(null, true)
+            } else {
+                callback(new Error('No origin header'), false)
+            }
+        },
+        credentials: true
+    },
     connectionStateRecovery: {
         maxDisconnectionDuration: 60 * 60 * 1000,
         skipMiddlewares: true,
     }
+})
+
+instrument(io, {
+    auth: false,
+    mode: 'development',
 })
 
 io.on('connection', (socket) => {
